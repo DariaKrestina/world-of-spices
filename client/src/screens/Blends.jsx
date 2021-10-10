bf9dcce884e17445d24837164b33cdc4ba51064c
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-10-10 11:57:06 -0400

    Finish css for Blends screen desktop

--- client/src/screens/Blends.jsx
+++ client/src/screens/Blends.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import './Blends.css'
 
 export default function Blends(props) {
   return (
-    <div>
+    <div className="blends">
       <h1>Your spice blend collection</h1>
       {props.blends.map(blend => (
-        <div key={`blend${blend.id}`}>
-          <p>{blend.name}</p>
-          <p>{blend.spices.map(spice => <p>{spice.name}</p>)}</p>
-          <Link to={`/blends/${blend.id}/edit`}><button>Update</button></Link>
-          <button onClick={()=>props.handleBlendDelete(blend.id)}>Delete</button>
+        <div className="blends-single-blend" key={`blend${blend.id}`}>
+          <div>
+            <h2>{blend.name}</h2>
+            <p>{blend.spices.map(spice => <span>{spice.name} </span>)}</p>
+          </div>
+          <div>
+            <Link to={`/blends/${blend.id}/edit`}><button><img src="https://i.imgur.com/DOqmnqd.png" alt="edit" width="40px"/></button></Link>
+            <button onClick={() => props.handleBlendDelete(blend.id)}><img src="https://i.imgur.com/lFrq8MB.png" alt="delete" width="40px" /></button>
+          </div>
         </div>
       ))}
-      <Link to='/blends/new'><button>Add new blend</button></Link>
+      <Link to='/blends/new'><button className="blends-add-button">Add new blend</button></Link>
     </div>
   )
 }
