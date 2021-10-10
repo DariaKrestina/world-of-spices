1f06fd1c9b4c2143a07ce05f99afda5d5a63d6a3
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-10-10 10:51:30 -0400

    Finish css for SpiceDetail desktop

--- client/src/screens/SpiceDetail.jsx
+++ client/src/screens/SpiceDetail.jsx
@@ -1,19 +1,32 @@
-import { useParams } from "react-router-dom"
-import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./SpiceDetail.css";
 
 export default function SpiceDetail(props) {
-  const { id } = useParams()
+  const { id } = useParams();
 
-  const spice = props.spices.find(spice => spice.id === Number(id))
-  return !spice ? (<h1>Loading...</h1>) : (
+  const spice = props.spices.find((spice) => spice.id === Number(id));
+  return !spice ? (
+    <h1>Loading...</h1>
+  ) : (
     <article>
       <h1>{spice.name}</h1>
-      <img alt="spice" src={spice.img_URL} width="50%" />
-      <p>{spice.description}</p>
-      <p>Flavor</p>
-      <p>{spice.flavor}</p>
-      <h4>Do you want to make a special mix of spices?</h4>
-      <Link to='/blends/new'><button>Create new blend</button></Link>
+      <div className="detail-img-and-description">
+          <img alt="spice" src={spice.img_URL} width="50%" />
+          <div className="detail-description-only">
+            <p>{spice.description}</p>
+            <div className="detail-flavor">
+              <span>Flavor</span>
+              <p>{spice.flavor}</p>
+            </div>
+          </div>
+      </div>
+      <div className="detail-button-and-description">
+        <h4>Do you want to make a special mix of spices?</h4>
+        <Link to="/blends/new">
+          <button>Create new blend</button>
+        </Link>
+      </div>
     </article>
-  )
+  );
 }
