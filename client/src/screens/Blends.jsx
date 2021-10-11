404687a090e9bf6fcba6469a9467f6ec939a4892
Daria Krestina <dariakrestina@Darias-MacBook-Pro.local>
2021-10-11 02:28:18 -0400

    Complete css for Blends screen mobile

--- client/src/screens/Blends.jsx
+++ client/src/screens/Blends.jsx
@@ -12,7 +12,7 @@ export default function Blends(props) {
             <h2>{blend.name}</h2>
             <p>{blend.spices.map(spice => <span>{spice.name} </span>)}</p>
           </div>
-          <div>
+          <div className="blends-icons">
             <Link to={`/blends/${blend.id}/edit`}><button><img src="https://i.imgur.com/DOqmnqd.png" alt="edit" width="40px"/></button></Link>
             <button onClick={() => props.handleBlendDelete(blend.id)}><img src="https://i.imgur.com/lFrq8MB.png" alt="delete" width="40px" /></button>
           </div>
